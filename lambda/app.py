22256b5d1570fb21a53ff2e40d35c40d4fa33dfc
root <root@Astrea.localdomain>
2025-07-25 11:07:19 +0800

    Add structured logging to Lambda

--- lambda/app.py
+++ lambda/app.py
@@ -1,6 +1,15 @@
 import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def handler(event, context):
+    logger.info(json.dumps({
+        "message": "Lambda triggered",
+        "event": event
+    }))
+    
     return {
         "statusCode": 200,
         "body": json.dumps({"message": "Hello from Mini Project 1!"})
